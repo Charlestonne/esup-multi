baf8fe5b1d17c1ed3b06d523f6d951d68dd79b50
Sebastian Linares <juanselindel@gmail.com>
2026-04-28 10:48:37 +0200

    angular.json.dist pour events

--- dev/user-backend-mocks/src/events/events.route.js
+++ dev/user-backend-mocks/src/events/events.route.js
@@ -4,4 +4,10 @@ const { eventsData } = require('./events.mock');
 
 router.get('/', (req, res) => res.json(eventsData));
 
+router.get('/:id', (req, res) => {
+  const event = eventsData.find(e => e.id === req.params.id);
+  if (!event) return res.status(404).json({ message: 'Not found' });
+  res.json(event);
+});
+
 module.exports = router;
